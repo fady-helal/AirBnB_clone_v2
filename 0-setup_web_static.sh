257807abdf72845df8ef6c8f07e564b11dbf8967
fadyhelal <fadyhelal8@gmail.com>
2024-07-14 01:54:53 +0300

    Executable!

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,14 +1,7 @@
-# craete a server
-if ! command -v nginx &> /dev/null; then
-    echo "Nginx could not be found, installing..."
-    sudo apt-get update
-    sudo apt-get install nginx -y
-    sudo ufw allow 'Nginx HTTP'
-else
-    echo "Nginx is already installed."
-fi
-
-# Create the required directories
+#!/usr/bin/env bash
+# this script setup web servers
+sudo apt-get update
+sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
@@ -16,8 +9,6 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-
-# Create a simple HTML file for testing
 sudo echo "<html>
   <head>
   </head>
@@ -25,11 +16,7 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create a new symbolic link
 sudo ln -sf  /data/web_static/releases/test/ /data/web_static/current
-
 sudo chown -R ubuntu:ubuntu /data/
-
 sudo sed -i  '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
